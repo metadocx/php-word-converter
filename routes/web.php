93f21fbbdaff47fab01f9b239e3c3b68d754457a
Benoit Gauthier <bgauthier@metadocx.com>
2023-01-05 09:47:12 +0000

    Initial version of word converter not ready for production

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Metadocx\Reporting\Converters\Word\WordConverter;
 
 Route::post("/Metadocx/Convert/Word", function(Request $request) {
 
-    return "@todo";
-
+    $oWordConverter = new WordConverter();    
+    $oWordConverter->loadOptions($request->input("ExportOptions"));    
+    $sFileName = $oWordConverter->convert($request->input("HTML"));
+    if ($sFileName !== false) {       
+        $headers = ["Content-Type"=> "application/octet-stream"];
+        return response()
+                ->download($sFileName, "Report.docx", $headers)
+                ->deleteFileAfterSend(true);
+    } 
 });
--- src/WordConverter.php
+++ src/WordConverter.php
@@ -6,4 +6,65 @@ use Illuminate\Support\Facades\Log;
 
 class WordConverter {
     
+    protected $_sOutputFileName = null;
+    protected $_aOptions = [];
+
+    public function convert($sContent) {
+        
+
+        $oWordDocument = new \PhpOffice\PhpWord\PhpWord();
+        $oDocumentSection = $oWordDocument->addSection();
+     
+        $doc = new \DOMDocument();
+        libxml_use_internal_errors(true);
+        $doc->loadHTML(base64_decode($sContent), LIBXML_NOERROR);
+        $doc->normalizeDocument();
+
+        \PhpOffice\PhpWord\Shared\Html::addHtml($oDocumentSection, $doc->saveHTML(), true);
+
+        $this->_sOutputFileName = storage_path("app/" . uniqid("Word") . ".docx");
+        $oWordDocument->save($this->_sOutputFileName, 'Word2007');
+
+        if (file_exists($this->_sOutputFileName)) {
+
+            return $this->_sOutputFileName;
+        } else {
+            return false;
+        }
+
+    }
+
+    public function loadOptions($options) {
+
+        $this->_aOptions = [];
+
+    }
+
+    public function __get(string $name) {
+        $name = str_replace("_", "-", $name);
+        return $this->_aOptions[$name];
+    }
+
+    public function __set(string $name, mixed $value) {
+        $name = str_replace("_", "-", $name);
+        $this->_aOptions[$name] = $value;
+        return $this;
+    }
+
+    private function toBool($value) {
+        if (is_bool($value)) {
+            return (bool) $value;
+        }
+
+        $value = strtolower(trim($value));
+
+        $aTrueValues = ["1","y","o","yes","true","oui","vrai","on","checked",true,1];
+        
+        if (in_array($value, $aTrueValues, true)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
